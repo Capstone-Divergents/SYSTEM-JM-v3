235bb013f00b1bd474d45f3744a35858498acba9
jmcamilo435 <jmcamilo435@JMCAMILO>
2018-01-03 21:16:52 +0800

    Employer View added.

---
+++ Findstaff/ucEmployerView.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace Findstaff
+{
+    partial class country
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label = new System.Windows.Forms.Label();
+            this.lblPrincipal = new System.Windows.Forms.Label();
+            this.lblCountry = new System.Windows.Forms.Label();
+            this.lblEmp = new System.Windows.Forms.Label();
+            this.lblEmpID = new System.Windows.Forms.Label();
+            this.lblViewEmp = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.compEmail = new System.Windows.Forms.Label();
+            this.contact = new System.Windows.Forms.Label();
+            this.compAdd = new System.Windows.Forms.Label();
+            this.forPrincipal = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.employer = new System.Windows.Forms.Label();
+            this.empID = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(422, 289);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(135, 19);
+            this.label4.TabIndex = 39;
+            this.label4.Text = "Company Email :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(467, 258);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 19);
+            this.label5.TabIndex = 38;
+            this.label5.Text = "Contact # :";
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.BackColor = System.Drawing.Color.Transparent;
+            this.label.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label.ForeColor = System.Drawing.Color.Black;
+            this.label.Location = new System.Drawing.Point(402, 227);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(155, 19);
+            this.label.TabIndex = 37;
+            this.label.Text = "Company Address :";
+            // 
+            // lblPrincipal
+            // 
+            this.lblPrincipal.AutoSize = true;
+            this.lblPrincipal.BackColor = System.Drawing.Color.Transparent;
+            this.lblPrincipal.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrincipal.ForeColor = System.Drawing.Color.Black;
+            this.lblPrincipal.Location = new System.Drawing.Point(410, 195);
+            this.lblPrincipal.Name = "lblPrincipal";
+            this.lblPrincipal.Size = new System.Drawing.Size(147, 20);
+            this.lblPrincipal.TabIndex = 36;
+            this.lblPrincipal.Text = "Foreign Principal :";
+            // 
+            // lblCountry
+            // 
+            this.lblCountry.AutoSize = true;
+            this.lblCountry.BackColor = System.Drawing.Color.Transparent;
+            this.lblCountry.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCountry.ForeColor = System.Drawing.Color.Black;
+            this.lblCountry.Location = new System.Drawing.Point(478, 163);
+            this.lblCountry.Name = "lblCountry";
+            this.lblCountry.Size = new System.Drawing.Size(79, 20);
+            this.lblCountry.TabIndex = 35;
+            this.lblCountry.Text = "Country :";
+            // 
+            // lblEmp
+            // 
+            this.lblEmp.AutoSize = true;
+            this.lblEmp.BackColor = System.Drawing.Color.Transparent;
+            this.lblEmp.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmp.ForeColor = System.Drawing.Color.Black;
+            this.lblEmp.Location = new System.Drawing.Point(468, 131);
+            this.lblEmp.Name = "lblEmp";
+            this.lblEmp.Size = new System.Drawing.Size(89, 20);
+            this.lblEmp.TabIndex = 34;
+            this.lblEmp.Text = "Employer :";
+            // 
+            // lblEmpID
+            // 
+            this.lblEmpID.AutoSize = true;
+            this.lblEmpID.BackColor = System.Drawing.Color.Transparent;
+            this.lblEmpID.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmpID.ForeColor = System.Drawing.Color.Black;
+            this.lblEmpID.Location = new System.Drawing.Point(445, 100);
+            this.lblEmpID.Name = "lblEmpID";
+            this.lblEmpID.Size = new System.Drawing.Size(112, 20);
+            this.lblEmpID.TabIndex = 33;
+            this.lblEmpID.Text = "Employer ID :";
+            // 
+            // lblViewEmp
+            // 
+            this.lblViewEmp.AutoSize = true;
+            this.lblViewEmp.BackColor = System.Drawing.Color.Transparent;
+            this.lblViewEmp.Font = new System.Drawing.Font("Century", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblViewEmp.ForeColor = System.Drawing.Color.Black;
+            this.lblViewEmp.Location = new System.Drawing.Point(25, 25);
+            this.lblViewEmp.Name = "lblViewEmp";
+            this.lblViewEmp.Size = new System.Drawing.Size(196, 28);
+            this.lblViewEmp.TabIndex = 40;
+            this.lblViewEmp.Text = "View Employer";
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.White;
+            this.btnClose.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnClose.FlatAppearance.BorderSize = 0;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.ForeColor = System.Drawing.Color.Black;
+            this.btnClose.Location = new System.Drawing.Point(510, 355);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(100, 38);
+            this.btnClose.TabIndex = 42;
+            this.btnClose.Text = "CLOSE";
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // compEmail
+            // 
+            this.compEmail.AutoSize = true;
+            this.compEmail.BackColor = System.Drawing.Color.Transparent;
+            this.compEmail.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.compEmail.ForeColor = System.Drawing.Color.Black;
+            this.compEmail.Location = new System.Drawing.Point(563, 289);
+            this.compEmail.Name = "compEmail";
+            this.compEmail.Size = new System.Drawing.Size(91, 19);
+            this.compEmail.TabIndex = 49;
+            this.compEmail.Text = "compEmail";
+            // 
+            // contact
+            // 
+            this.contact.AutoSize = true;
+            this.contact.BackColor = System.Drawing.Color.Transparent;
+            this.contact.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.contact.ForeColor = System.Drawing.Color.Black;
+            this.contact.Location = new System.Drawing.Point(563, 258);
+            this.contact.Name = "contact";
+            this.contact.Size = new System.Drawing.Size(62, 19);
+            this.contact.TabIndex = 48;
+            this.contact.Text = "contact";
+            // 
+            // compAdd
+            // 
+            this.compAdd.AutoSize = true;
+            this.compAdd.BackColor = System.Drawing.Color.Transparent;
+            this.compAdd.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.compAdd.ForeColor = System.Drawing.Color.Black;
+            this.compAdd.Location = new System.Drawing.Point(563, 227);
+            this.compAdd.Name = "compAdd";
+            this.compAdd.Size = new System.Drawing.Size(81, 19);
+            this.compAdd.TabIndex = 47;
+            this.compAdd.Text = "compAdd";
+            // 
+            // forPrincipal
+            // 
+            this.forPrincipal.AutoSize = true;
+            this.forPrincipal.BackColor = System.Drawing.Color.Transparent;
+            this.forPrincipal.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.forPrincipal.ForeColor = System.Drawing.Color.Black;
+            this.forPrincipal.Location = new System.Drawing.Point(563, 195);
+            this.forPrincipal.Name = "forPrincipal";
+            this.forPrincipal.Size = new System.Drawing.Size(97, 20);
+            this.forPrincipal.TabIndex = 46;
+            this.forPrincipal.Text = "forPrincipal";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.Black;
+            this.label8.Location = new System.Drawing.Point(563, 163);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(66, 20);
+            this.label8.TabIndex = 45;
+            this.label8.Text = "country";
+            // 
+            // employer
+            // 
+            this.employer.AutoSize = true;
+            this.employer.BackColor = System.Drawing.Color.Transparent;
+            this.employer.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.employer.ForeColor = System.Drawing.Color.Black;
+            this.employer.Location = new System.Drawing.Point(563, 131);
+            this.employer.Name = "employer";
+            this.employer.Size = new System.Drawing.Size(77, 20);
+            this.employer.TabIndex = 44;
+            this.employer.Text = "employer";
+            // 
+            // empID
+            // 
+            this.empID.AutoSize = true;
+            this.empID.BackColor = System.Drawing.Color.Transparent;
+            this.empID.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.empID.ForeColor = System.Drawing.Color.Black;
+            this.empID.Location = new System.Drawing.Point(563, 100);
+            this.empID.Name = "empID";
+            this.empID.Size = new System.Drawing.Size(59, 20);
+            this.empID.TabIndex = 43;
+            this.empID.Text = "empID";
+            // 
+            // country
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
+            this.Controls.Add(this.compEmail);
+            this.Controls.Add(this.contact);
+            this.Controls.Add(this.compAdd);
+            this.Controls.Add(this.forPrincipal);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.employer);
+            this.Controls.Add(this.empID);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.lblViewEmp);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.lblPrincipal);
+            this.Controls.Add(this.lblCountry);
+            this.Controls.Add(this.lblEmp);
+            this.Controls.Add(this.lblEmpID);
+            this.Name = "country";
+            this.Size = new System.Drawing.Size(1118, 450);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label lblPrincipal;
+        private System.Windows.Forms.Label lblCountry;
+        private System.Windows.Forms.Label lblEmp;
+        private System.Windows.Forms.Label lblEmpID;
+        private System.Windows.Forms.Label lblViewEmp;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label compEmail;
+        private System.Windows.Forms.Label contact;
+        private System.Windows.Forms.Label compAdd;
+        private System.Windows.Forms.Label forPrincipal;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label employer;
+        private System.Windows.Forms.Label empID;
+    }
+}
